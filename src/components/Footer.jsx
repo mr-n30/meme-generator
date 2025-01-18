e6a0f00b705b44b7a64acfe5403803efebff1845
archie <archie@localhost>
2025-01-18 10:35:37 -0600

    Updated code. Not finished yet.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer>
-      &copy; 2025 <a href="https://github.com/mr-n30" className="footer-href">mr-n30</a>. All rights reserved
+      <small>&copy; 2025 <a href="https://github.com/mr-n30" target="_blank" className="footer-href">mr-n30</a>. All rights reserved.</small>
     </footer>
   )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,23 +1,37 @@
+import { useState } from "react"
+
 export default function Main() {
+  const [memeText, setMemeText] = useState({top: "Shut up", bottom: "and take my money!"})
+
+  function handleChange(event) {
+    const {value, name} = event.currentTarget
+    setMemeText(prev => ({
+      ...prev,
+      [name]: value
+    }))
+  }
+
   return (
     <main className="main-meme">
-      <form className="form-meme">
+      <form className="form-meme" onSubmit={event => event.preventDefault()}>
         <div className="form-group">
           <label htmlFor="top-text">
             Top Text 
           </label>
-          <input id="top-text" className="form-input-meme" placeholder="shut up" type="text" name="top_text" />
+          <input id="top-text" className="form-input-meme" placeholder="shut up" type="text" name="top" onChange={handleChange} value={memeText.top} />
         </div>
         <div className="form-group">
           <label htmlFor="bottom-text">
             Bottom Text
           </label>
-          <input id="bottom-text" className="form-input-meme" placeholder="and take my money!" type="text" name="bottom_text" />
+          <input id="bottom-text" className="form-input-meme" placeholder="and take my money!" type="text" name="bottom" onChange={handleChange} value={memeText.bottom} />
         </div>
         <button className="form-button-meme">Get a new meme image 🖼️</button>
       </form>
       <div className="meme-image-container">
-        <img src="https://wallpapers.com/images/featured/funny-meme-pictures-ppzthb74p3b686b9.jpg" alt="Meme Background Image" className="img-meme" />
+        <span className="top">{memeText.top}</span>
+        <span className="bottom">{memeText.bottom}</span>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHz4zJFmNhrBqYd-tbdiw_og5CrCQcv207NQ&s" alt="Meme Background Image" className="img-meme" />
       </div>
     </main>
   )
